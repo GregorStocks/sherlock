6b9ab335cf0719063e7c79b57c0520a58cd778dd
Kurper <kurper@gmail.com>
2010-07-31 13:15:03 -0700

    Added the start of some scripts that don't run, and the most basic UI possible (a square at (50, 50)).

---
+++ scripts/base.py
@@ -0,0 +1,13 @@
+class AutoRegister(type):
+	def __init__(cls, name, bases, dct):
+		print "pretend we're registering this now: %s" % cls
+		super(AutoRegister, cls).__init__(name, bases, dct)
+
+class Entity(object):
+	__metaclass__ = AutoRegister
+
+class Item(Entity):
+	def poop(self):
+		print "poop"
+
+Item().poop()
---
+++ scripts/pants.py
@@ -0,0 +1,4 @@
+class Pants(Clothes):
+	icon = 'data/pants.png'
+	slot = Clothes.PANTS_SLOT
+
